5cdecf62fb8c5f664f542ecdf76e7af5c9fcd1a0
Evan Duffy <elduffy@wpi.edu>
2018-11-19 18:33:02 -0500

    The thing is done we gucci

--- app/src/main/java/edu/wpi/clockdemo/MainActivity.java
+++ app/src/main/java/edu/wpi/clockdemo/MainActivity.java
@@ -1,32 +1,20 @@
 package edu.wpi.clockdemo;
 
-import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.app.AlarmManager;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.TimePicker;
-import android.widget.ToggleButton;
 
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        inst = this;
-    }
-    private static MainActivity inst;
-
-    public static MainActivity instance() {
-        return inst;
-    }
+    private int alarm_index = 1;
 
 
     @Override
@@ -35,20 +23,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    PendingIntent pendingIntent;
+    ////// On Click Handlers //////
 
-    public void onMainClick(View v) {
+    public void onTPClick(View v) {
         TimePicker alarmTP = findViewById(R.id.timePicker);
+        this.setAlarm(alarmTP.getHour(), alarmTP.getMinute());
+    }
+
+    public void onHourClick(View v) {
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date()); // Set the time to now
+        time.add(Calendar.HOUR, 1);
+        this.setAlarm(time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE));
+    }
+
+    public void onHalfHourClick(View v) {
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date()); // Set the time to now
+        time.add(Calendar.MINUTE, 30);
+        this.setAlarm(time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE));
+    }
+
+    public void onTwoHourClick(View v) {
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date()); // Set the time to now
+        time.add(Calendar.HOUR, 2);
+        this.setAlarm(time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE));
+    }
+
+    public void onEightHourClick(View v) {
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date()); // Set the time to now
+        time.add(Calendar.HOUR, 8);
+        this.setAlarm(time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE));
+    }
+
+    ////// Set Alarm //////
+
+    /**
+     * Set an alarm for a given time
+     * @param hour The hour for the alarm
+     * @param minute The minute for the alarm
+     */
+    private void setAlarm(int hour, int minute) {
         Intent createAlarmIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
-        createAlarmIntent.putExtra(AlarmClock.EXTRA_HOUR, alarmTP.getHour());
-        createAlarmIntent.putExtra(AlarmClock.EXTRA_MINUTES, alarmTP.getMinute());
+        createAlarmIntent.putExtra(AlarmClock.EXTRA_HOUR, hour);
+        createAlarmIntent.putExtra(AlarmClock.EXTRA_MINUTES, minute);
+        createAlarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE,  "Demo Alarm " + this.alarm_index++);
+
+        Switch toggle = findViewById(R.id.switch1);
+
+        createAlarmIntent.putExtra(AlarmClock.EXTRA_SKIP_UI, toggle.isChecked());
 
         getApplicationContext().startActivity(createAlarmIntent);
     }
 
-
-    public void setAlarmText(String text){
-        TextView alarmTextView = findViewById(R.id.alarmText);
-        alarmTextView.setText(text);
-    }
 }
